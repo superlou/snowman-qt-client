79158a4642a3d7e4116a28a7a327aaa90b2c7d9e
superlou <lousimons@gmail.com>
2016-12-10 21:23:32 -0500

    Added hacky connection to snowmix

--- snowman_qt.py
+++ snowman_qt.py
@@ -6,6 +6,7 @@ from PyQt5.QtCore import QObject, QUrl, pyqtProperty, pyqtSignal, pyqtSlot
 from PyQt5.QtGui import QGuiApplication
 from PyQt5.QtQml import QQmlApplicationEngine, qmlRegisterType, QQmlComponent, QQmlListProperty
 from PyQt5.QtQuick import QQuickItem, QQuickView
+from manager_connection import ManagerConnection
 
 
 class MainBusButton(QQuickItem):
@@ -16,18 +17,36 @@ class MainBusButton(QQuickItem):
 class MainBus(QQuickItem):
     def __init__(self, parent=None):
         super().__init__(parent)
+        self._manager = None
+
+    @pyqtProperty(ManagerConnection)
+    def manager(self):
+        return self._manager
+
+    @manager.setter
+    def manager(self, newManager):
+        self._manager = newManager
 
     @pyqtSlot(int)
     def setPreview(self, channel):
-        print("preview:", channel)
+        self.manager.send({'action': 'set_preview', 'feed': channel})
 
     @pyqtSlot(int)
     def setProgram(self, channel):
-        print("program:", channel)
+        self.manager.send({'action': 'set_program', 'feed': channel})
 
+
+def on_manager_update(self, type, value):
+    if type == 'preview':
+        self.preview_feed = value
+    elif type == 'program':
+        self.program_feed = value
+    elif type == 'active_dsks':
+        self.active_dsks = value
 
 def main():
     # os.environ["QML_IMPORT_TRACE"] = "1"
+    manager = ManagerConnection(5555, 5556, on_manager_update)
     app = QGuiApplication(argv)
 
     qmlRegisterType(MainBus, 'Snowman', 1, 0, 'MainBus')
@@ -39,6 +58,7 @@ def main():
     view.setSource(QUrl.fromLocalFile(sourceFile))
 
     mainBus = view.rootObject().findChild(QQuickItem, 'mainBus')
+    mainBus.setProperty('manager', manager)
     mainBus.setProperty('programChannel', 0)
     mainBus.setProperty('previewChannel', 1)
 
